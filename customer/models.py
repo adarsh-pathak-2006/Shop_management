537b577b02c3c7446055aabc6654d97222133d6c
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-05 00:22:28 +0530

    added models in both the apps

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from inventory.models import Product
 
-# Create your models here.
+class Sale(models.Model):
+    product_name=models.ForeignKey(Product)
+    quantity=models.PositiveIntegerField()
+    price_per_piece=models.PositiveIntegerField()
+    discount=models.PositiveIntegerField()
+    total_price=models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        price=self.price_per_piece * self.quantity
+        self.total_price=price - self.discount
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product_name
+
+
+
