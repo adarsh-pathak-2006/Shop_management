c7a5eb22fc5f66f0bf3080ce46e9d745deeace45
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-05 00:09:51 +0530

    initial commit

---
+++ inventory/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Category(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name=models.CharField(max_length=300)
+    stock=models.PositiveIntegerField(default=0)
+    is_avaliable=models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.stock > 0:
+            self.is_avaliable==True
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.product_name} of {self.category.name}"
+
